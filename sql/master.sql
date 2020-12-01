81339eaaf81b0a4ed1c13f905b2b3dd080fbeef3
Gregory Pohlod <greg@pohlod.ca>
2020-12-01 07:52:45 -0800

    updated master sql to contain hardware table sql info

--- sql/master.sql
+++ sql/master.sql
@@ -120,3 +120,51 @@ SELECT * FROM users;
 SELECT * FROM authorities;
 SELECT * FROM tickets;
 SELECT * FROM comments;
+
+-- Hardware	Type   
+DROP TABLE IF EXISTS hardwareType;
+
+CREATE TABLE hardwareType(
+    hardwareTypeId INT AUTO_INCREMENT PRIMARY KEY NOT NULL ,
+	hardwareTypeDescription VARCHAR(100) NOT NULL ,
+);
+
+INSERT INTO hardwareType (hardwareTypeDescription) 
+		VALUES ('Dell Inspiron Desktop'),
+			   ('Dell 560 Laptop'),
+			   ('Lenovo Yoga 1090 Laptop'),
+			   ('Brother Printer'),
+			   ('IPhone 12'),
+			   ('IPhone 10');
+
+
+
+-- Hardware		   
+DROP TABLE IF EXISTS hardwareAssignment;
+
+CREATE TABLE hardwareAssignment(
+    hardwareId INT AUTO_INCREMENT PRIMARY KEY NOT NULL ,
+	hardwareTypeName VARCHAR(100) NOT NULL ,
+	status VARCHAR(30) NOT NULL ,
+	usernameAssignedTo VARCHAR(30) NOT NULL ,
+    dateAssigned TIMESTAMP NOT NULL ,
+    dateReturned TIMESTAMP,
+    FOREIGN KEY(usernameAssignedTo) REFERENCES users(username),
+	FOREIGN KEY(hardwareTypeName) REFERENCES hardwareType(hardwareTypeDescription)
+);
+
+INSERT INTO hardwareAssignment (hardwareTypeName, status, usernameAssignedTo, dateAssigned, dateReturned) 
+		VALUES ('Dell Inspiron Desktop', 'Assigned', 'user', '2019-12-07 06:42:21', null),
+			   ('Dell 560 Laptop', 'Assigned', 'catFan', '2020-11-07 06:42:21', null),
+			   ('Dell 560 Laptop', 'Assigned', 'employee', '2020-11-07 06:42:21', null),
+			   ('Dell 560 Laptop', 'Assigned', 'admin', '2020-11-07 06:42:21', null),
+			   ('Lenovo Yoga 1090 Laptop', 'Returned', 'user', '2020-07-07 06:42:21', '2019-12-07 06:42:21'),
+			   ('Brother Printer', 'Assigned', 'employee', '2020-07-07 06:42:21', null),
+			   ('IPhone 12', 'Lost', 'employee', '2020-07-07 06:42:21', null),
+			   ('IPhone 12', 'Assigned', 'admin', '2020-07-07 06:42:21', null);
+			   
+
+
+
+
+			   
